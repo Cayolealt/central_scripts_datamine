6af151f96d06855b588ceae9fe1cfc4ca50cd3ce
Cayo L. Tarrago <cayolealt@gmail.com>
2025-02-12 06:43:04 -0300

    adicao do html e css da configuracao e criacao do objeto script

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+class Script{
+    
+    constructor(){
+        this.nome = ''
+        this.path = ''
+    }
+
+    adicionar(){
+        alert("vamos adicionar um script.")
+    }
+
+    excluir(){
+        alert("Vamos excluir um script")
+    }
+}
+
+var script = new Script()
